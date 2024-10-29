77e52de60a71cce9f76065dd8dad8490c93852a7
Shivansh Sharma <shivansh.report@gmail.com>
2024-10-30 02:01:58 +0530

    Initial prototyping done

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,15 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"os"
 
+	"github.com/shivanshsharma8834/coolbox/cmd/info"
+	"github.com/shivanshsharma8834/coolbox/cmd/net"
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -36,6 +35,11 @@ func Execute() {
 	}
 }
 
+func addSubCommandPalettes() {
+	rootCmd.AddCommand(net.NetCmd)
+	rootCmd.AddCommand(info.InfoCmd)
+}
+
 func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
@@ -46,6 +50,6 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	addSubCommandPalettes()
 }
-
-
